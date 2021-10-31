cb0b137c0d113887eb1ab9ac85ab090f6402a9b0
phantombass <tmkeaton@ngu.edu>
2021-10-31 17:27:10 -0400

    Pokemon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -124,7 +124,7 @@ namespace XVLauncher.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.5.9.
+        ///   Looks up a localized string similar to 1.0.
         /// </summary>
         internal static string LauncherVersion {
             get {
